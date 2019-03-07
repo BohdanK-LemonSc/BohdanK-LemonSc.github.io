ba31235e9cfb4686aba2a90fa87e0bd58b09e829
BohdanK-LemonSc <bodya226@ukr.net>
2019-03-07 20:06:22 +0200

    upload files

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
 $(document).ready(function () {
-    // $(".owl-carousel").owlCarousel({
-    //         loop: false,
-    //         margin: 10,
-    //         nav: true,
-    //         items: 1,
-    //         mouseDrag: false,
-    //         smartSpeed: 700,
-    //         autoplay: false,
-    //     autoplayTimeout:3000,
-    //     rewind: true
-    //     }
-    // );
+    $(".ipad-slider .owl-carousel").owlCarousel({
+            animateIn: 'fadeIn',
+            animateOut: 'fadeOutRight',
+            loop: true,
+            margin: 0,
+            nav: false,
+            items: 1,
+            mouseDrag: false,
+            smartSpeed: 1500,
+            autoplay: true,
+            autoplayTimeout: 3000
+        }
+    );
 
+    $(".clients .owl-carousel").owlCarousel({
+            loop: true,
+            margin: 0,
+            nav: true,
+            dots: true,
+            navText: "",
+            items: 1,
+            mouseDrag: true,
+            smartSpeed: 1500,
+            autoplay: true,
+            autoplayTimeout: 993000
+        }
+    );
 
     //********** -- mobile menu behaviour --------
 
@@ -35,4 +49,152 @@ $(document).ready(function () {
     });
 
 //**********************************************************
+    //*************** circle progress bar ************
+    var circle_viewed = false;
+    var circle_scroll = $('.circle');
+    var offset = circle_scroll.offset();
+
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat(offset.top)) - parseFloat($(window).height()))) && (!(circle_viewed))) { //if page skrolling
+            $('.circle-1').circleProgress({
+                size: 160,
+                startAngle: .3,
+                value: 0.9,
+                thickness: 3,
+                fill: {color: "#ff0036"}
+            }).on('circle-animation-progress', function (event, progress) {
+                $(this).find('strong').html(parseInt(90 * progress) + '<i>%</i>');
+            });
+
+            $('.circle-2').circleProgress({
+                size: 160,
+                startAngle: .6,
+                value: 0.8,
+                thickness: 3,
+                fill: {color: "#ff0036"}
+            }).on('circle-animation-progress', function (event, progress) {
+                $(this).find('strong').html(parseInt(80 * progress) + '<i>%</i>');
+            });
+
+            $('.circle-3').circleProgress({
+                size: 160,
+                startAngle: .9,
+                value: 0.70,
+                thickness: 3,
+                fill: {color: "#ff0036"}
+            }).on('circle-animation-progress', function (event, progress) {
+                $(this).find('strong').html(parseInt(70 * progress) + '<i>%</i>');
+            });
+
+            $('.circle-4').circleProgress({
+                size: 160,
+                startAngle: .3,
+                value: 0.9,
+                thickness: 3,
+                fill: {color: "#ff0036"}
+            }).on('circle-animation-progress', function (event, progress) {
+                $(this).find('strong').html(parseInt(90 * progress) + '<i>%</i>');
+            });
+
+            circle_viewed = true;
+        }
+    });
+
+    var services_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.services').offset().top)) - parseFloat($(window).height()))) && (!(services_viewed))) { //if page skrolling
+            $('.services').addClass('animated fadeInUp slow');
+            services_viewed = true;
+        }
+    });
+    var about_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.about').offset().top)) - parseFloat($(window).height()))) && (!(about_viewed))) { //if page skrolling
+            $('.about').addClass('animated fadeInUp slow');
+            about_viewed = true;
+        }
+    });
+    var portfolio_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.portfolio').offset().top)) - parseFloat($(window).height()))) && (!(portfolio_viewed))) { //if page skrolling
+            $('.portfolio').addClass('animated fadeInUp slow');
+            portfolio_viewed = true;
+        }
+    });
+    var products_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.our-products').offset().top)) - parseFloat($(window).height()))) && (!(products_viewed))) { //if page skrolling
+            $('.our-products').addClass('animated fadeInUp slow');
+            products_viewed = true;
+        }
+    });
+    var clients_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.clients').offset().top)) - parseFloat($(window).height()))) && (!(clients_viewed))) { //if page skrolling
+            $('.clients').addClass('animated fadeInUp slow');
+            clients_viewed = true;
+        }
+    });
+    var team_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.team').offset().top)) - parseFloat($(window).height()))) && (!(team_viewed))) { //if page skrolling
+            $('.team').addClass('animated fadeInUp slow');
+            team_viewed = true;
+        }
+    });
+    var skills_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.skills').offset().top)) - parseFloat($(window).height()))) && (!(skills_viewed))) { //if page skrolling
+            $('.skills').addClass('animated fadeInUp slow');
+            skills_viewed = true;
+        }
+    });
+    var plans_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.plans').offset().top)) - parseFloat($(window).height()))) && (!(plans_viewed))) { //if page skrolling
+            $('.plans').addClass('animated fadeInUp slow');
+            plans_viewed = true;
+        }
+    });
+    var twitter_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.twitter').offset().top)) - parseFloat($(window).height()))) && (!(twitter_viewed))) { //if page skrolling
+            $('.twitter').addClass('animated fadeInUp slow');
+            twitter_viewed = true;
+        }
+    });
+    var blog_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.blog').offset().top)) - parseFloat($(window).height()))) && (!(blog_viewed))) { //if page skrolling
+            $('.blog').addClass('animated fadeInUp slow');
+            blog_viewed = true;
+        }
+    });
+    var follow_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('.follow').offset().top)) - parseFloat($(window).height()))) && (!(follow_viewed))) { //if page skrolling
+            $('.follow').addClass('animated fadeInUp slow');
+            follow_viewed = true;
+        }
+    });
+    var footer_viewed = false;
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat($('footer').offset().top)) - parseFloat($(window).height()))) && (!(footer_viewed))) { //if page skrolling
+            $('footer').addClass('animated fadeInUp slow');
+            footer_viewed = true;
+        }
+    });
+
+    var $page = $('html, body');
+    $('header nav a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1500);
+        return false;
+    });
+
+    if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) || /Edge\/\d./i.test(navigator.userAgent))
+    {$('.about .hexagon').each(function () {
+        $(this).addClass('ie_edge_style');
+    })}
 });
